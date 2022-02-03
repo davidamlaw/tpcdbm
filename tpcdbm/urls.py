c3548524e07bb8a236d41be46326c1872b8aa33e
David Amlaw <47164592+davidamlaw@users.noreply.github.com>
2022-02-03 16:48:00 -0500

    Add files via upload

--- tpcdbm/urls.py
+++ tpcdbm/urls.py
@@ -16,16 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
-admin.site.site_header = "Price Center DBM Admin"
-admin.site.site_title = "Price Center DBM Admin"
-admin.site.index_title = "Welcome to Price Center DBM Admin"
+admin.site.site_header = "Day Center DBM Admin"
+admin.site.site_title = "Day Center DBM Admin"
+admin.site.index_title = "Welcome to Day Center DBM Admin"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', auth_views.LoginView.as_view(template_name='login.html'),name='login'),
     path('home/', views.HomePage.as_view(template_name='home.html'), name='home'),
+    path('clients/', include('clients.urls', namespace='clients')),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
